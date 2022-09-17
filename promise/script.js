2fda1cd67e0132df4021fc63b7c63671364ae5bc
ZGQ <gangqingzhao321@163.com>
2022-09-17 19:00:46 +0800

    chore:完成代码

--- promise/script.js
+++ promise/script.js
@@ -1,121 +1,117 @@
 class Promise {
     constructor(executor) {
-        // 添加属性
-        this.PromiseState = 'pending'
-        this.PromiseResult = null
-        this.callBacks = []
+        this.promiseResult = null
+        this.promiseState = 'pending'
+        this.callBack = []
         const self = this
-        // resolve 函数
         function resolve(data) {
-            // 状态改变一次
-            if (self.PromiseState!=='pending') {
+            // console.log(self)
+            if (self.promiseState!=='pending') {
                 return
             }
-            // 修改对象的状态(promiseState)，设置结果值(promiseResult)
-            self.PromiseState = 'fulfilled'
-            self.PromiseResult =  data
-            // 调用成功的回调函数
-            // self.callBack.onResolved()
-            // if (self.callBack.onResolved) {
-            //     self.callBack.onResolved(data)
-            // }
-            // 异步执行
-            setTimeout(() => {
-                self.callBacks.forEach(item => {
-                    item.onResolved()
-                })
-            })
+            self.promiseResult = data
+            self.promiseState = 'fulfilled'
+            self.callBack.forEach(element => {
+                element.onResolved()
+            });
         }
-        // reject 函数
         function reject(data) {
-            // 状态改变一次
-            if (self.PromiseState!=='pending') {
+            if (self.promiseState!=='pending') {
                 return
             }
-            self.PromiseResult = data
-            self.PromiseState = 'rejected'
-            // self.callBack.onRejected()
-            // if (self.callBack.onRejected) {
-            //     self.callBack.onRejected(data)
-            // }
-            // 异步执行
-            setTimeout(()=> {
-                self.callBacks.forEach(item => {
-                    item.onRejected()
-                })
-            })
+            self.promiseResult = data
+            self.promiseState = 'reject'
+            self.callBack.forEach(element => {
+                element.onRejected()
+            });
+
         }
-        // 同步调用执行器函数
         try {
             executor(resolve,reject)
         } catch (error) {
-            // 修改promise对象为失败
             reject(error)
         }
     }
     //then方法
     then(onResolved,onRejected) {
+        if (typeof onResolved !== 'function') {
+            onResolved = value => value 
+        }
+        if (typeof onRejected !== 'function') {
+            onRejected = reason => {throw reason} 
+        }
         const self = this
-        // 判断回调函数参数
-        if (typeof onRejected !== 'funtion') {
-            onRejected = reason => {
-                throw reason
-            }
-        }
-        if(typeof onResolved !== 'function') {
-            onResolved = value => value
-        }
         return new Promise((resolve,reject) => {
-            //封装函数
-            function callback(type) {
+            if(this.promiseState==='fulfilled') {
                 try {
-                    let result = type(self.PromiseResult)
-                    // 获取函数的执行结果,如果结果是Promise的一个实例
-                    if (result instanceof Promise) {
-                        // 如果是promise
-                        result.then(v => {
+                    let res = onResolved(this.promiseResult)
+                    if(res instanceof Promise) {
+                        res.then(v => {
                             resolve(v)
-                        },r=>{
+                        },r => {
                             reject(r)
                         })
-                    }else{
-                        // 结果状态为成功
-                        resolve(result)
+                    }else {
+                        resolve(res)
                     }
                 } catch (error) {
                     reject(error)
                 }
             }
-                //调用回调函数
-        if (this.PromiseState==='fulfilled') {
-            setTimeout(() => {
-                callback(onResolved)
-            })
-        }
-        if (this.PromiseState==='rejected') {
-            // let result = onRejected(this.PromiseResult)
-            setTimeout(() => {
-                callback(onRejected)   
-            })    
-        }
-        // 判断pending的状态
-        if (this.PromiseState==='pending') {
-            // 保存回调函数,这种方法保存回调函数只会执行一次
-            // this.callBack = {
-                // onResolved,
-                // onRejected
-            // }
-            this.callBacks.push({
-                onResolved:function(){
-                    // 执行成功的回调函数
-                    callback(onResolved)
-                },
-                onRejected:function(){
-                    callback(onRejected)              
+            if(this.promiseState==='reject') {
+                try {
+                    let res = onRejected(this.promiseResult)
+                    if(res instanceof Promise) {
+                        res.then(v => {
+                            resolve(v)
+                        },r => {
+                            reject(r)
+                        })
+                    }else {
+                        resolve(res)
+                    }
+                } catch (error) {
+                    reject(error)   
                 }
-            })
-        }
-        })
+            }   
+            if(this.promiseState==='pending') {                
+                this.callBack.push({
+                    onResolved:() => {
+                        try {
+                            let res = onResolved(this.promiseResult)
+                            if(res instanceof Promise) {
+                                res.then(v => {
+                                    resolve(v)
+                                },r => {
+                                    reject(r)
+                                })
+                            }else {
+                                resolve(res)
+                            }
+                        } catch (error) {
+                            reject(error)
+                        }
+                    },
+                    onRejected: () => { 
+                        try {
+                            let res = onRejected(this.promiseResult)
+                            if(res instanceof Promise) {
+                                res.then(v => {
+                                    resolve(v)
+                                },r => {
+                                    reject(r)
+                                })
+                            }else {
+                                resolve(res)
+                            }
+                        } catch (error) {
+                            reject(error)   
+                        }
+                    }
+                })
+                              
+            }
+        })     
     }
     catch(onRejected){
         return this.then(undefined,onRejected)
